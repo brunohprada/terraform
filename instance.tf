e3800651e1f736f1423f634d6ef778d1528bf27d
brunohprada <brunohprada@hotmail.com>
2018-05-25 22:36:40 -0300

    primeiro código de terraform

---
+++ instance.tf
@@ -0,0 +1,37 @@
+resource "google_compute_instance" "default" {
+  name = "test"
+  machine_type = "f1-micro"
+  zone = "southamerica-east1-c"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+      initialize_params {
+          image = "debian-cloud/debian-9"
+      }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+
+  }
+
+  network_interface {
+      network = "defaul"
+
+      access_config {
+          // Ephemeral IP
+      }
+  }
+
+  metadata {
+      foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+      scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
